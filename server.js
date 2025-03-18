3978c2045b13bfd598857f1d68ef24c71a6cb10e
Ega Ananta <egaananta2004@.com>
2025-03-18 09:38:30 +0700

    ganti port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
-const PORT = 5001;
+const PORT = 3000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
